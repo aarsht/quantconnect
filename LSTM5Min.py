4cbb11d0b4c065d3418f1f0b0f965e7d2afc9e2f
aarsht <40334187+aarsht@users.noreply.github.com>
2019-05-22 18:43:43 +0530

    Update LSTM5Min.py

--- LSTM5Min.py
+++ LSTM5Min.py
@@ -18,6 +18,7 @@ class LSTM5Min(QCAlgorithm):
         self.indicator_warmup = 30
         self.equity = 'AAPL'
         self.AddEquity(self.equity, Resolution.Minute)
+        # setting data resolution to 5 minutes.
         consolidator = TradeBarConsolidator(self.resolution_period)
         consolidator.DataConsolidated += self.OnDataConsolidated
         self.SubscriptionManager.AddConsolidator(self.equity, consolidator)
@@ -41,34 +42,35 @@ class LSTM5Min(QCAlgorithm):
         self.count = 0
         self.tech_data = pd.DataFrame()
         self.scaled_data = pd.DataFrame()
-        self.y = []
-        
+     
 
     def OnData(self, data):
 
         pass
-    
+   
+    # The event will be triggered each 5 min interval to pump the data.
     def OnDataConsolidated(self, sender, bar):
         self.count+=1
+        # warming up for model fitting.
         if self.IsWarmingUp:
+            # warming up upto max indicator warming up period.
             if self.count > self.indicator_warmup:
                 if self.tech_data.empty:
                     self.tech_data = pd.DataFrame([[bar.Open,bar.Close,bar.Low,bar.High,bar.Volume,float(self.mfi.ToString()),float(self.rsi.ToString()),float(self.ema.ToString()),float(self.bb.ToString()),float(self.ppo.ToString()),float(self.cci.ToString()),float(self.dema.ToString()),float(self.tema.ToString())]],columns=['open','close','low','high','volume','mfi','rsi','ema','bb','ppo','cci','dema','tema'])
                 else:
                     self.tech_data = self.tech_data.append(pd.DataFrame([[bar.Open,bar.Close,bar.Low,bar.High,bar.Volume,float(self.mfi.ToString()),float(self.rsi.ToString()),float(self.ema.ToString()),float(self.bb.ToString()),float(self.ppo.ToString()),float(self.cci.ToString()),float(self.dema.ToString()),float(self.tema.ToString())]],columns=['open','close','low','high','volume','mfi','rsi','ema','bb','ppo','cci','dema','tema']))  
+                    # model fitting at the last time step of warm up period.
                     if self.count == self.model_warmup:
                         self.Debug('tech data shape:')
                         self.Debug(self.tech_data.shape)
+                        # difference between non indicator data.
                         self.tech_data.iloc[:,:5] = self.tech_data.iloc[:,:5] - self.tech_data.iloc[:,:5].shift(1)
                         self.tech_data.dropna(how='any',inplace=True)
-                        #remove outliers at high value side
-                        #self.Debug(self.tech_data.head())
                         for column in self.tech_data:
                             self.scaled_data[column] = np.tanh((self.tech_data[column].values-np.mean(self.tech_data[column].values))/np.std(self.tech_data[column].values))
-                        #self.Debug(self.scaled_data.describe())
-                        #self.Debug(self.scaled_data.head())
                         self.x_train = np.array(self.scaled_data.tail(-1))
                         self.x_train = np.reshape(self.x_train, (self.x_train.shape[0],1,13))
+                        # categorizing target data
                         self.y_train = pd.cut(self.tech_data['close'].shift(-1),[-np.inf,0,np.inf],labels=[0,1])
                         self.y_train = np.array(self.y_train.dropna())
                         self.model = Sequential()
@@ -83,9 +85,10 @@ class LSTM5Min(QCAlgorithm):
                         # Compiling the RNN
                         self.model.compile(optimizer='Adam',loss='binary_crossentropy', metrics=['acc','mse'])
                         # Fitting to the training set
-                        history_callback = self.model.fit(self.x_train,self.y_train,epochs=7,batch_size=32,verbose=2)
+                        history_callback = self.model.fit(self.x_train,self.y_train,epochs=10,batch_size=32,verbose=2)
                         self.Debug(history_callback.history["loss"])
         else:
+            # Fitting model further due to time limit of 10 minutes at each time step.
             if self.count == self.model_warmup+1:
                 history_callback1 = self.model.fit(self.x_train,self.y_train,epochs=10,batch_size=32,verbose=2)
                 self.Debug(history_callback1.history["loss"])
@@ -94,6 +97,7 @@ class LSTM5Min(QCAlgorithm):
                 self.Debug(y_sample.describe)
             x_live = pd.DataFrame([[bar.Open,bar.Close,bar.Low,bar.High,bar.Volume,float(self.mfi.ToString()),float(self.rsi.ToString()),float(self.ema.ToString()),float(self.bb.ToString()),float(self.ppo.ToString()),float(self.cci.ToString()),float(self.dema.ToString()),float(self.tema.ToString())]],columns=['open','close','low','high','volume','mfi','rsi','ema','bb','ppo','cci','dema','tema'])
             history_tradebar = self.History(self.Symbol(self.equity), self.resolution_period+1, Resolution.Minute)
+            # difference between non indicator data.
             x_live.iloc[:,:5] = x_live.iloc[:,:5] - history_tradebar.iloc[0,:]
             #applying same tanh transformation on live data
             for column in x_live:
